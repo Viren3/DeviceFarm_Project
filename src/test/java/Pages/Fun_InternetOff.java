e4e4a90d2536e96cadcdbccbf39d1b1781f5ea48
Viren3 <viren.chauhan@adrixus.com>
2022-10-31 19:17:44 +0530

    Restructure Framework

--- src/test/java/Pages/Fun_InternetOff.java
+++ src/test/java/Pages/Fun_InternetOff.java
@@ -1,15 +1,11 @@
 package Pages;
-
-import Utilities.BaseClass;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
 import io.appium.java_client.android.AndroidDriver;
-import Utilities.ExtendsReportClass;
+import Utilities.BaseClass;
+import Pages.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-
 
 public class Fun_InternetOff extends BaseClass {
     AndroidDriver mobileDriver;
@@ -20,21 +16,20 @@ public class Fun_InternetOff extends BaseClass {
         mobileDriver = remoteDriver;
     }
 
-//    public void testDemo(){webDriver.get(baseUrlStr);}
 
     public void addNumber() throws InterruptedException {
         Elements el = new Elements(mobileDriver);
 
         mobileDriver.findElement(el.clickonTextbox).click();
         Thread.sleep(3000);
-//        ExtendsReportClass.getTest().log(LogStatus.PASS, "Click on Back icon in Browser ", "User should be able to click on Back icon.");
         mobileDriver.findElement(By.className("android.widget.EditText")).sendKeys("7567543247");
         Thread.sleep(4000);
         log.info("testing 2");
         mobileDriver.hideKeyboard();
-//        mobileDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView")).click();
-//        Thread.sleep(2000);
-//        mobileDriver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup")).click();
-
+        Thread.sleep(2000);
+        mobileDriver.findElement(el.clickOnPrivacyCheckBox).click();
+        Thread.sleep(2000);
+        mobileDriver.findElement(el.btnGetOtp).click();
+        Thread.sleep(2000);
     }
 }
--- src/test/java/Utilities/BaseClass.java
+++ src/test/java/Utilities/BaseClass.java
@@ -1,16 +1,16 @@
 package Utilities;
 
-import Pages.Fun_InternetOff;
-import com.aventstack.extentreports.ExtentReports;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import Pages.Fun_InternetOff;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
@@ -27,24 +27,17 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 
-public class BaseClass extends ExtentReports {
+public class BaseClass {
 
     public static Logger log = LogManager.getLogger(Fun_InternetOff.class.getName());
     public Properties properties;
+    public static WebDriver webDriver;
     public static AndroidDriver<AndroidElement> mobileDriver;
-    /*Appium Start manually*/
-//    AppiumDriver<MobileElement> mobileDriver;
     public AppiumDriverLocalService appiumService;
     public AppiumServiceBuilder builder;
-    /* Remove comment while use appium start manually */
-//    public static String baseUrlStr = "http://127.0.0.1:4723/wd/hub";
-
-
     @BeforeTest
     public void mobileSetup() throws MalformedURLException {
-        /*For AWS Devices*/
-//        final String URL_STRING = "http://127.0.0.1:4723/wd/hub";
-//        URL url = new URL(URL_STRING);
+
         File f = new File("Application_build");
         File fileName = new File(f, "yoCricket-v23.09.01.apk");
 
@@ -63,26 +56,15 @@ public class BaseClass extends ExtentReports {
 
         String appiumServiceUrl = appiumService.getUrl().toString();
         System.out.println("Appium Service Address : - " + appiumServiceUrl);
-
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy_Nexus_API");
         cap.setCapability(MobileCapabilityType.APP, fileName.getAbsolutePath());
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         cap.setCapability("platformName", "Android");
         cap.setCapability("autoGrantPermissions", true);
-        /*For AWS Devices*/
-//        cap.setCapability("deviceName", "Android Emulator");
-//        cap.setCapability("appPackage","com.amazonaws.devicefarm.android.referenceapp");
-//        cap.setCapability("appActivity","com.amazonaws.devicefarm.android.referenceapp.Activities.MainActivity");
         cap.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-        /*For AWS Devices*/
-//        mobileDriver = new AndroidDriver<AndroidElement>(url, cap);
         mobileDriver = new AndroidDriver<AndroidElement>(new URL(appiumServiceUrl), cap);
-        mobileDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        /* Remove comment while use appium start manually */
-//        URL url = new	  URL("http://127.0.0.1:4723/wd/hub");
-//        mobileDriver = new AppiumDriver<MobileElement>(url,cap);
+        mobileDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     public static boolean checkIfServerIsRunnning(int port) {
@@ -101,6 +83,7 @@ public class BaseClass extends ExtentReports {
         }
         return isServerRunning;
     }
+
     public String getScreenshotPath(String TestCaseName) throws IOException, InterruptedException {
         String dateName = new SimpleDateFormat("yyyy.MM.dd.hh.mm.ss").format(new Date());
         File source = mobileDriver.getScreenshotAs(OutputType.FILE);
@@ -112,6 +95,7 @@ public class BaseClass extends ExtentReports {
     public void teradown(){
         mobileDriver.quit();
     }
+
     @AfterSuite(alwaysRun = true)
     public void mobileTearDown() {
         if (appiumService != null) {
